ca86ca2afed3c056e666d005a2ea39ca6f334f6d
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-03-05 13:49:15 +0100

    added page handling to remove .html on url
    by using ajax to handle .html url and without refreshing page

--- Resources/javascript/index.js
+++ Resources/javascript/index.js
@@ -7,3 +7,34 @@ function responsive() {
         nav.className = 'navlist';
     }
 }
+
+function aboutMe() {
+    window.location = 'https://www.tiril.tech/aboutme.html';
+}
+
+$(document).ready(function() {
+    // Intercept clicks on links
+    $('a').on('click', function(e) {
+        // Check if the link is external
+        if ($(this).attr('href').startsWith('https')) {
+            return true; // Allow default behavior for external links
+        }
+
+        e.preventDefault(); // Prevent default link behavior
+
+        // Get the href attribute
+        var href = $(this).attr('href');
+
+        // Use pushState to change the URL without reloading the page
+        history.pushState(null, null, href);
+
+        // Load the content of the clicked page using AJAX
+        $('#content').load(href + ' #content');
+    });
+
+    // Handle back/forward navigation
+    window.onpopstate = function() {
+        $('#content').load(location.pathname + ' #content');
+    };
+});
+
