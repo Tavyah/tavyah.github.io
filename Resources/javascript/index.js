f948145d9ff10e90db5a09d4ac0c820a610cfcb7
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-03-05 13:54:03 +0100

    Update index.js
    Removed the page handler for now, it's messing up links

--- Resources/javascript/index.js
+++ Resources/javascript/index.js
@@ -11,30 +11,3 @@ function responsive() {
 function aboutMe() {
     window.location = 'https://www.tiril.tech/aboutme.html';
 }
-
-$(document).ready(function() {
-    // Intercept clicks on links
-    $('a').on('click', function(e) {
-        // Check if the link is external
-        if ($(this).attr('href').startsWith('https')) {
-            return true; // Allow default behavior for external links
-        }
-
-        e.preventDefault(); // Prevent default link behavior
-
-        // Get the href attribute
-        var href = $(this).attr('href');
-
-        // Use pushState to change the URL without reloading the page
-        history.pushState(null, null, href);
-
-        // Load the content of the clicked page using AJAX
-        $('#content').load(href + ' #content');
-    });
-
-    // Handle back/forward navigation
-    window.onpopstate = function() {
-        $('#content').load(location.pathname + ' #content');
-    };
-});
-
